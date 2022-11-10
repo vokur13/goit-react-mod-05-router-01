67d83215ed9f6e912cbf4b57ba58e06c6339b44d
vokur13 <vokur13@gmail.com>
2022-11-10 18:26:32 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { Toaster } from 'react-hot-toast';
+import { Routes, Route } from 'react-router-dom';
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+      </Routes>
+      <GlobalStyle />
+      <Toaster position="top-right" reverseOrder={false} />
+    </>
   );
 };
